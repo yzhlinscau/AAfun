808a3cd486419ddaa4e47f2ef3011406db5d91bf
Yuanzhen Lin <yzhlinscau@163.com>
2016-05-21 09:47:21 +0800

    Update mc.ped.R

--- R/mc.ped.R
+++ R/mc.ped.R
@@ -4,7 +4,7 @@ function(ped){
   p1a<-levels(ped[,2])
   p1b<-levels(ped[,3])
   p1<-c(p1a,p1b)
-  ped1<-data.frame(p1,p2=NA,p3=NA)
+  ped1<-data.frame(p1,p2=0,p3=0)
   names(ped1)<-names(ped)
   ped2<-rbind(ped1,ped)
   ped2   
