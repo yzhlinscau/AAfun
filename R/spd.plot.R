135c86f317e9ee6d473a755976854bcfd8d580ed
Yuanzhen Lin <yzhlinscau@163.com>
2017-11-15 13:55:16 +0800

    Update spd.plot.R

--- R/spd.plot.R
+++ R/spd.plot.R
@@ -1,7 +1,7 @@
 spd.plot<-
 function(object,type="data",p.lbls=NULL,key.unit=NULL,   
              x.unit=NULL,y.unit=NULL,na=NULL,
-             color.p=NULL,...){
+             color.p=NULL,mtitle=NULL,...){
   #require(plyr)  
   par(mar=c(4,4,2,2), cex.main=1)
   if(is.null(color.p)) color.p=terrain.colors
@@ -13,12 +13,12 @@ function(object,type="data",p.lbls=NULL,key.unit=NULL,
   }
   if(type=="variogram"){object=object[,-4];for(i in 1:2) object[,i]=object[,i]+1} 
   
-  Ncol<-max(object[2])
+  ncol<-max(object[2])
   lbls<-names(object[3])
   lbls2<-paste(lbls,key.unit,sep="\n(")
   lbls2<-paste(lbls2,"",sep=")")
-  object.1<-as.vector(object[,3])
-  df <-matrix(object.1,nrow=Ncol,byrow=TRUE)
+  object.1<-(object[,3])
+  df <-matrix(object.1,nrow=ncol,byrow=TRUE)
   if(is.null(na)) na=1
   if(na==0)  df[is.na(df)]<-0.0001  # reduce effects of NA value in data 
   
@@ -50,10 +50,10 @@ function(object,type="data",p.lbls=NULL,key.unit=NULL,
   
   if(is.null(p.lbls)) p.lbls=" " else p.lbls=paste(": ",p.lbls)
   if(is.null(key.unit)) lbls2=lbls else lbls2=lbls2
-  
+  if(is.null(mtitle)) mtitle=paste("The Topography of ",lbls, p.lbls) else mtitle=mtitle
   #windows(10,8)
   filled.contour(x, y, df, color.palette=color.p, 
-                 plot.title = title( main=paste("The Topography of ",lbls, p.lbls), 
+                 plot.title = title( main=mtitle, 
                                      xlab="Col", ylab="Row"), 
                  plot.axes = { 
                    axis(1,x.axis) 
@@ -63,4 +63,5 @@ function(object,type="data",p.lbls=NULL,key.unit=NULL,
   )
   #abline(v=0, h=seq(1, max(y), by=1),lty=2,col="grey75") 
   #abline(h=0, v=seq(1, max(x), by=1),lty=2,col="gray75")
+  
 }
