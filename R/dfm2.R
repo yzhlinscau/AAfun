ec9a157a65c93114f19872dbcc766727da2cf787
Yuanzhen Lin <yzhlinscau@163.com>
2018-07-18 19:31:38 +0800

    Add files via upload

---
+++ R/dfm2.R
@@ -0,0 +1,29 @@
+#' @title Tree spacing trial dataset2
+#' 
+#' @description A dataset of spacing trials of Pinus radiata
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name PrSpa
+#' @usage data(dfm2)
+#' @format one dataset with 17 variables
+#' \describe{
+#'  \item{TreeID}{Tree Sample code}
+#'  \item{Mum}{Tree mum code}
+#'  \item{Dad}{Tree dad code} 
+#'  \item{Spacing}{3 levels}
+#'  \item{Rep}{Replication code}
+#'  \item{Plot}{Plot code}
+#'  \item{dj}{a numeric vector}
+#'  \item{dm}{a numeric vector}
+#'  \item{wd}{a numeric vector}
+#'  \item{h1}{a numeric vector}
+#'  \item{h2}{a numeric vector}
+#'  \item{h3}{a numeric vector}
+#'  \item{h4}{a numeric vector}
+#'  \item{h5}{a numeric vector}
+#'  \item{dis}{a numeric vector}
+#'  \item{lt}{a numeric vector}
+#'  \item{str}{a numeric vector}
+#' }
+NULL
