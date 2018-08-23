82b89a9023b6e68fde89a19ea3f02843ecee949c
Yuanzhen Lin <yzhlinscau@163.com>
2018-08-23 09:48:44 +0800

    Update met.corr.R

--- R/met.corr.R
+++ R/met.corr.R
@@ -1,5 +1,5 @@
 met.corr <-
-function(object,site,faN=1,kn=NULL){ # ,faRS=1
+function(object,site,faN=1,kn=NULL,plot=TRUE){ # ,faRS=1
   #require(amap)
   #require(corrgram)
   
@@ -17,10 +17,10 @@ function(object,site,faN=1,kn=NULL){ # ,faRS=1
   
   #vect2<-summary(object)$varcomp$component[(n+faRS):((1+faN)*n+faRS-1)]
   vect2<-varcomp1[(n+1):((1+faN)*n)]
-  t.var<-matrix(vect2,nrow=n)
+  L.var<-matrix(vect2,nrow=n)
   
-  wt.var<-t.var%*%t(t.var)+w.var
-  df<-wt.var
+  wL.var<-L.var%*%t(L.var)+w.var
+  df<-wL.var
   
   for(i in 1:(n-1)){
     for(j in 2:n){
@@ -29,6 +29,8 @@ function(object,site,faN=1,kn=NULL){ # ,faRS=1
     }
     i<-i+1
   }
+  rownames(df)<-c(paste("S",levels(site),sep='')) # might be some problem
+  colnames(df)<-c(paste("S",levels(site),sep=''))  
   
   df.2<-df
   
@@ -40,26 +42,26 @@ function(object,site,faN=1,kn=NULL){ # ,faRS=1
     i<-i+1
   }
   diag(df)<-1
-  rownames(df)<-c(paste("S",levels(site),sep=''))
-  colnames(df)<-c(paste("S",levels(site),sep=''))  
   
-  chcluster <- amap::hclusterpar(na.omit(df), method="manhattan")
-  #windows(10,8)
-  plot(chcluster, main="Fig.1 Cluster of different sites",hang=-1)  #  labels=F
-  rect.hclust(chcluster, k=kn)
-  cat("Site cluster results:\n")
-  print(tree.id<-cutree(chcluster,k=kn))
-  
-  if(n<16){
+  if(plot==TRUE){
+    chcluster <- amap::hclusterpar(na.omit(df), method="manhattan")
     #windows(10,8)
-    corrgram::corrgram(df, type="cor",order=T, lower.panel=panel.pie,
-               upper.panel=panel.conf, text.panel=panel.txt,
-               main="Fig.2 Correlogram of different sites")
-  }
+    plot(chcluster, main="Fig.1 Cluster of different sites",hang=-1)  #  labels=F
+    rect.hclust(chcluster, k=kn)
+    cat("Site cluster results:\n")
+    print(tree.id<-cutree(chcluster,k=kn))
+  
+    if(n<16){
+      #windows(10,8)
+      corrgram::corrgram(df, type="cor",order=T, lower.panel=panel.pie,
+                         upper.panel=panel.conf, text.panel=panel.txt,
+                         main="Fig.2 Correlogram of different sites")
+    }
+  }  
   
   cat("\nCov\\Var\\Corr matrix\n\n")
-  rownames(df.2)<-c(paste("S",levels(site),sep=''))
-  colnames(df.2)<-c(paste("S",levels(site),sep=''))
+  #rownames(df.2)<-c(paste("S",levels(site),sep=''))
+  #colnames(df.2)<-c(paste("S",levels(site),sep=''))
   print(round(df.2,3))
   cat("\n")
 }
